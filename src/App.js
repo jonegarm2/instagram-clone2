9a9ec67a9621f9406519b349e00ced64bb1b3904
Jon Garman <jonegarm2@gmail.com>
2018-07-24 16:38:55 -0700

    props and headers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Post from './components/Post';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to my nightmare</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit your life <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          These are getting shorter and shorter
-        </p>
+      <div>
+        <Header />
+        <div>
+          <Post />
+        </div>
       </div>
     );
   }
 }
-
 export default App;
